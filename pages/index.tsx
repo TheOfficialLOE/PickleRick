11bccc9460bcc13b96069778742b5e3c3c18f9f7
MaxShady <max.shady1611@gmail.com>
2022-07-04 16:33:03 +0430

    Remove gif temporarily

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import PickleRickComponent from "../components/PickleRickComponent";
+import PickleRick from "../components/PickleRickComponent";
 
 
 const Main = () => {
     return (
         <div className="h-screen md:mx-5 md:py-6 grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2">
             <p>I'm Pickle <br/> Rick</p>
-            <PickleRickComponent
+            <PickleRick
                 className="row-span-full md:row-span-1 md:rounded-3xl my-auto h-full md:object-cover place-self-center"/>
         </div>
     );
