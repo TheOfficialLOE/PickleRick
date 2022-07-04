8fca84586ecea5f9c2d22429c6b4cb52a5f8a4cb
MaxShady <max.shady1611@gmail.com>
2022-07-04 13:53:10 +0430

    Install Tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+
+// measures: 501 + 627
+
+const Main = () => {
+    return (
+        <div>
+        <h1>Hello World</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            quidem, quisquam.
+        </p>
+        </div>
+    );
+};
+
+export default Main;
